dfdcfc0ad605886728b27844a949f841c29e0116
namrata.chowdhury05@gmail.com <namrata.chowdhury05@gmail.com>
2024-03-28 21:32:36 +0530

    code for jenins

---
+++ src/test/java/com/javatechie/devops/jenkinscicd/JenkinsCiCdApplicationTests.java
@@ -0,0 +1,28 @@
+package com.javatechie.devops.jenkinscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+@WebMvcTest
+class JenkinsCiCdApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testGreetingsEndpoint() throws Exception {
+		String name ="Namrata Chowdhury";
+		mockMvc.perform(MockMvcRequestBuilders.get("/greetings/{name}",name))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().string("Hello" + name + "Congratulations you have successfully completed Jenkins CI/CD Demo"));
+	}
+
+}
